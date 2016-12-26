bdd4c97b9745e495563ff0d534ec1c95a9e1d9d4
Ignat Insarov <kindaro@gmail.com>
2016-12-26 18:54:30 +0300

    Add a (kinda) test.

--- test/Main.hs
+++ test/Main.hs
@@ -1,3 +1,17 @@
 module Main where
 
-main = return ()
+import Text.BracketsValidator
+
+main = do
+    test "(meow)" (Validation (State {position = 4, status = True}) [])
+    test "(meow]" (Validation (State {position = 4, status = False}) [CSquare,ORound])
+
+test expression expectation = do
+        putStrLn $ "Testing " ++ (show expression) ++ " ..."
+        putStrLn $ "Expectation: " ++ (show expectation)
+        putStrLn $ "Result: " ++ (show result)
+        case (result == expectation) of
+            True -> putStrLn "Test passed."
+            False -> putStrLn "Test failed."
+
+    where result = (parser.lexer) expression
