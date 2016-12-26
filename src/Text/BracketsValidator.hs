974aa1c245868377d7585b71b0442883a639ddcb
Ignat Insarov <kindaro@gmail.com>
2016-12-26 16:06:30 +0300

    A draft of (a part of) a monadic parser.

--- src/Text/BracketsValidator.hs
+++ src/Text/BracketsValidator.hs
@@ -1,9 +1,28 @@
 module Text.BracketsValidator where
 
-data Validation = Validation [Symbol] | Error | Ok deriving (Show)
-
 data Symbol = ORound | OSquare | OCurled | CRound | CSquare | CCurled | Blank String
     deriving (Eq, Show, Read)
+
+isOpen x = case x of
+    ORound -> True
+    OSquare -> True
+    OCurled -> True
+    _ -> False
+
+isClose x = case x of
+    CRound -> True
+    CSquare -> True
+    CCurled -> True
+    _ -> False
+
+isBlank x = case x of
+    Blank _ -> True
+    _ -> False
+
+isMatching ORound CRound = True
+isMatching OSquare CSquare = True
+isMatching OCurled CCurled = True
+isMatching _ _ = False
 
 lexer :: String -> [Symbol]
 lexer [] = []
@@ -19,21 +38,43 @@ lexer (x:xs)
         _ -> proceed $ Blank (x:[])
     where proceed = (: lexer xs)
 
-validate = show . validator (Validation []) . (lexer)
+insert :: Symbol -> [Symbol] -> Validation [Symbol]
+insert (Blank _) a = pure a
+insert s a
+    | isOpen s = pure (s:a)
+    | (not . null) a && s `isMatching` (head a) = pure (tail a)
+    | otherwise = impure (s:a)
 
-validator :: Validation -> [Symbol] -> Validation
-validator Error _ = Error
-validator (Validation []) [] = Ok
-validator v [] = Error
-validator (Validation []) (x:xs)
-    | x == ORound || x == OSquare || x == OCurled
-        = validator (Validation [x]) xs
-    | x == CRound || x == CSquare || x == CCurled
-        = Error
-    | otherwise = validator (Validation []) xs
-validator (Validation v@(w:ws)) (x:xs)
-    | x == ORound || x == OSquare || x == OCurled
-        = validator (Validation (x:v)) xs
-    | (w == ORound && x == CRound) || (w == OSquare && x == CSquare) || (w == OCurled && x == CCurled)
-        = validator (Validation ws) xs
-    | otherwise = Error
+data State = State { position :: Integer, status :: Bool }
+    deriving (Eq, Read, Show)
+
+data Validation x = Validation State x
+    deriving (Eq, Read, Show)
+
+instance Monoid State where
+    mempty = State { position = 1, status = True }
+    x `mappend` y
+        | status x && status y = State { position = position x + position y, status = True }
+        | otherwise = State { position = position x + position y, status = False }
+
+instance Functor Validation where
+    fmap f (Validation s a) = Validation s (f a)
+
+instance Applicative Validation where
+    pure x = Validation mempty x
+    (Validation s f) <*> (Validation t x) = Validation (s `mappend` t) (f x)
+
+taint (Validation s a) = Validation (s { status = False }) a
+impure x = taint $ pure x
+
+instance Monad Validation where
+    (Validation s x) >>= f
+        | status s == False = impure (unf x) -- Do nothing.
+        | otherwise = s `into` f x
+        -- Maybe I don't need this part.
+        where unpack (Validation s a) = a
+              unf = unpack . f
+              into s (Validation t y) = Validation (s `mappend` t) y
+
+parser :: Validation [Int] -> [Symbol] -> Validation [Int]
+parser = undefined
